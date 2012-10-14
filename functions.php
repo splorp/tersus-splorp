e538c11b6679761c042fd1ac472699d6db114177
Grant Hutchinson <grant@splorp.com>
2012-10-13 22:52:24 -0600

    Fixed feed validation error

--- functions.php
+++ functions.php
@@ -3,16 +3,11 @@
  * @package WordPress
  * @subpackage Tersus
  */
-?>
-
-<?php
 
 // Define Child Theme Constants
 
 	$theme_name = 'splorp';
-
 	$theme_data = get_theme_data( get_theme_root() . '/' . $theme_name . '/style.css');
-
 	define('CHILD_THEME_URI', $theme_data['URI']);
 	define('CHILD_THEME_NAME', $theme_data['Name']);
 	define('CHILD_THEME_AUTHOR', $theme_data['Author']);
