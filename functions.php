2f997bfc354b6e9fa6bb6939ea961e8edc2957b6
Grant Hutchinson <grant@splorp.com>
2013-10-25 17:00:04 -0600

    Removed unused constant

--- functions.php
+++ functions.php
@@ -10,7 +10,6 @@
 	$theme_data = get_theme_data( get_theme_root() . '/' . $theme_name . '/style.css');
 	define('CHILD_THEME_URI', $theme_data['URI']);
 	define('CHILD_THEME_NAME', $theme_data['Name']);
-	define('CHILD_THEME_AUTHOR', $theme_data['Author']);
 	define('CHILD_THEME_VERSION', trim($theme_data['Version']));
 	define('CHILD_THEME_DESCRIPTION', trim($theme_data['Description']));
 
